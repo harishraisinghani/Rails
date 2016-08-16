ca5b17f46395d4ca6996d3f34102ed5174ee5a58
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 02:10:20 +0000

    Add easy navigation links and ability to delete a post

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 LighthouseForum::Application.routes.draw do
   
-  get 'posts',          to: 'posts#index'
-  get 'posts/new',      to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id',      to: 'posts#show', as: 'post'
-  post 'posts',         to: 'posts#create'
-  patch 'posts/:id',    to: 'posts#update'
+  # get 'posts',          to: 'posts#index'
+  # get 'posts/new',      to: 'posts#new'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id',      to: 'posts#show', as: 'post'
+  # post 'posts',         to: 'posts#create'
+  # patch 'posts/:id',    to: 'posts#update'
+
+  resources :posts #this replaces all of the commented code above
+  
 end
