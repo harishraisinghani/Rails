eb74a4ca60d4698c52d520d311d1766e8f544bcd
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 01:51:14 +0000

    Add functionality to edit a post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 LighthouseForum::Application.routes.draw do
   
-  get 'posts',      to: 'posts#index'
-  get 'posts/new',  to: 'posts#new'
-  get 'posts/:id',  to: 'posts#show'
-  post 'posts',     to: 'posts#create'
+  get 'posts',          to: 'posts#index'
+  get 'posts/new',      to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id',      to: 'posts#show', as: 'post'
+  post 'posts',         to: 'posts#create'
+  patch 'posts/:id',    to: 'posts#update'
 end
