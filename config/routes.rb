f3c8e33bd86b36e052fe59370f8f03e60ab01afa
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 00:39:18 +0000

    Refactor individual static post routes to dynamic posts#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+LighthouseForum::Application.routes.draw do
+  
   get 'posts', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show'
+ 
+
 end
