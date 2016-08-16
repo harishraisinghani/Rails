0fc6270d4bfa12aa9e4e4e0240fb724369073a48
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 01:42:10 +0000

    Add a new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LighthouseForum::Application.routes.draw do
   
-  get 'posts', to: 'posts#index'
-  get 'posts/:id', to: 'posts#show'
- 
-
+  get 'posts',      to: 'posts#index'
+  get 'posts/new',  to: 'posts#new'
+  get 'posts/:id',  to: 'posts#show'
+  post 'posts',     to: 'posts#create'
 end
